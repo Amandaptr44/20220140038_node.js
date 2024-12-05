5a8c942d1f1fbb6abe9a9d103b7c1ce7f2378a15
github_Amandaptr44 <amanda.putri.ft22@mail.umy.ac.id>
2024-12-05 17:13:12 +0700

    STYLING_PAWC_038

--- app.js
+++ app.js
@@ -1,56 +1,79 @@
-const express = require('express'); 
-const app = express(); 
- 
-const todoRoutes = require('./routes/tododb.js'); 
-require('dotenv').config(); 
-const port = process.env.PORT; 
-const db = require('./database/db'); 
-const expressLayouts = require('express-ejs-layouts') 
-const session = require('express-session'); 
-// Mengimpor middleware 
-const authRoutes = require('./routes/authRoutes'); 
-const { isAuthenticated } = require('./middlewares/middleware.js'); 
- 
-app.use(expressLayouts); 
-app.use(express.json()); 
- 
-app.use('/todos', todoRoutes); 
-app.set('view engine', 'ejs'); 
- 
-app.use(express.urlencoded({ extended: true })); 
- 
-// Konfigurasi express-session 
-app.use(session({ 
-    secret: process.env.SESSION_SECRET || 'your_secret_key', // Gunakan secret key yang aman 
-    resave: false, 
-    saveUninitialized: false, 
-    cookie: { secure: false } // Set ke true jika menggunakan HTTPS 
-})); 
-app.use('/', authRoutes); 
- 
- 
-app.get('/', isAuthenticated, (req, res) => { 
-    res.render('index', { 
-        layout: 'layouts/main-layout' 
-    }); 
-}); 
- 
-app.get('/contact', isAuthenticated, (req, res) => { 
-    res.render('contact', { 
-        layout: 'layouts/main-layout' 
-    }); 
-}); 
- 
-app.get('/todo-view', isAuthenticated, (req, res) => { 
-    db.query('SELECT * FROM todos', (err, todos) => { 
-        if (err) return res.status(500).send('Internal Server Error'); 
-        res.render('todo', { 
-            layout: 'layouts/main-layout', 
-            todos: todos 
-        }); 
-    }); 
-}); 
- 
-app.listen(port, () => { 
-    console.log(`Server running at http://localhost:${port}/`); 
+const express = require('express');
+const app = express();
+const path = require('path'); // Tambahkan ini
+const todoroutes = require('./routes/tododb.js');
+require('dotenv').config();
+const port = process.env.PORT;
+const db = require('./database/db');
+//const expressLayouts = require('express-ejs-layouts')
+const session = require('express-session');
+// Mengimpor middleware
+const authRoutes = require('./routes/authRoutes');
+const { isAuthenticated } = require('./middlewares/middleware.js');
+
+
+app.use(express.json());
+app.use('/todos',todoroutes);
+app.set('view engine', 'ejs');
+
+//app.use(expressLayouts);
+app.use(express.urlencoded({ extended: true }));
+
+// Konfigurasi express-session
+app.use(session({
+    secret: process.env.SESSION_SECRET || 'your_secret_key', // Gunakan secret key yang aman
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // Set ke true jika menggunakan HTTPS
+}));
+
+
+// Middleware untuk file statis
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Atur view engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use('/', authRoutes);
+
+
+
+app.get('/login', (req, res) => {
+    console.log('Rendering login page with login-layouts');
+    res.render('login', { layout:"layouts/login-layouts" });
+});
+
+
+
+app.get('/', isAuthenticated, (req, res) =>{
+    res.render('index', {
+        layout: 'layouts/main-layout'
+    });
+});
+
+
+
+app.get('/contact', isAuthenticated, (req, res) =>{
+    res.render('contact',  {
+        layout: 'layouts/main-layout'
+    });
+});
+
+
+
+app.get('/todo', isAuthenticated, (req, res) => {
+    db.query('SELECT * FROM todos', (err, todos) => {
+        if (err) return res.status(500).send('Internal Server Error');
+        res.render('todo', {
+            layout: 'layouts/main-layout',
+            todos: todos
+        });
+    });
+});
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
 });
--- database/db.js
+++ database/db.js
@@ -1,11 +1,13 @@
 const mysql = require('mysql2');
 require('dotenv').config();
+
 const connection = mysql.createConnection({
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME
 });
+
 connection.connect((err) => {
     if (err) {
         console.error('Error connecting to the database:', err);
@@ -13,4 +15,5 @@ connection.connect((err) => {
     }
     console.log('Connected to the MySQL database.');
 });
+
 module.exports = connection;
